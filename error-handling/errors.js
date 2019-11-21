a9db2103704a760d69f050b021add09253d9f04a
Jessica Bailey <JessBailey@Jessicas-MacBook-Air.local>
2019-11-21 09:35:36 +0000

    error folder and file created

--- error-handling/errors.js
+++ error-handling/errors.js
@@ -3,6 +3,7 @@ exports.invalidMethods = (req, res, next) => {
   };
 
   exports.serverError = (err, req, res, next) => {
+    console.log(err);
     res.status(500).send({ msg: "Internal Server Error" });
   };
 
