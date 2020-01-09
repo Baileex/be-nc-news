c27f32fe58f71edd9f15a433c7ce3865c12a282a
Jessica Bailey <JessBailey@Jessicas-Air.home>
2020-01-09 19:10:38 +0000

    added cors to project

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const {
   psqlErrors,
   customErrors
 } = require("./error-handling/errors");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
